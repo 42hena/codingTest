5233c85f352a7e807790fff5a85c6f8bcb3ec435
42hena <somnium1108@gmail.com>
2021-08-08 01:13:32 +0900

    s5 1316 implementation

--- Implementation/S5_1316.py
+++ Implementation/S5_1316.py
@@ -1,11 +1,12 @@
-#fail
+#S5 1316 그룹 단어 체커
+#success
 N = int(input())
 
 
 arr = []
 for i in range(N):
     arr.append(input())
-print(arr)
+# print(arr)
 
 sum = 0
 for i in range(N):
@@ -15,7 +16,10 @@ for i in range(N):
         if count[ord(arr[i][j]) - ord('a')] == 0:
             count[ord(arr[i][j]) - ord('a')] += 1
         else:
-            break
+            if arr[i][j - 1] == arr[i][j]:
+                count[ord(arr[i][j]) - ord('a')] += 1
+            else:
+                break
         if j == len(arr[i]) - 1:
             sum += 1
 print(sum)
