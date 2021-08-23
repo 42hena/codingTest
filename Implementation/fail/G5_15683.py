32667f499f27d7cc6d0d33b7b7d0c6b0963e364c
42hena <somnium1108@gmail.com>
2021-08-24 01:01:58 +0900

    fail

---
+++ Implementation/fail/G5_15683.py
@@ -0,0 +1,36 @@
+N, M = map(int, input().split())
+
+graph = []
+
+for i in range(N):
+    graph.append(list(map(int, input().split())))
+
+print(graph)
+
+def func1(graph, i, j):
+    
+    while True:
+        j += 1
+        if graph[i][j] == 6:
+            return
+        if j >= M:
+            return 
+        graph[i][j] = '#'
+        
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+            func1(graph, i, j)
+
+for i in range(N):
+    for j in range(M):
+        print(graph[i][j], end=" ")
+    print()
+        # if graph[i][j] == 2:
+
+        # if graph[i][j] == 3:
+
+        # if graph[i][j] == 4:
+
+        # if graph[i][j] == 5:
