fa904e212fadf3f11bf4238dbca4477e19a3a062
42hena <somnium1108@gmail.com>
2022-01-14 00:08:40 +0900

    2022-01-14 [Programmers] 12954 x만큼 간격이 있는 n개의 숫자 <python>

---
+++ Programmers/lv1/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,9 @@
+def solution(x, n):
+    print([0] * 0)
+    if x > 0:
+        answer = [i for i in range(x, x*n + 1, x)]
+    elif x < 0:
+        answer = [i for i in range(x, x*n - 1, x)]
+    else:
+        answer = [0] * n
+    return answer
