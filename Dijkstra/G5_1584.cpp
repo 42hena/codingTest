adb1e88ae9bf82c11f1869ca685c540263ed89f9
42hena <somnium1108@gmail.com>
2022-02-21 23:30:00 +0900

    2022-02-21 [Baekjoon] 게임 <c++>

--- Dijkstra/G5_1584.cpp
+++ Dijkstra/G5_1584.cpp
@@ -59,7 +59,7 @@ int main()
     
     for (int i = 0 ; i <= 500 ; ++i)
         for (int j = 0 ; j <= 500 ; ++j)
-            dist[i][j] = 10000000;
+            dist[i][j] = 2147384647;
 
     int x1, y1, x2, y2;
     cin >> n;
