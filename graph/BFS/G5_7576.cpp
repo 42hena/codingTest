6743087999d60cc4a13afd4cfe19b149f95b53b4
42hena <somnium1108@gmail.com>
2022-02-28 22:43:35 +0900

    Feat : 2022-02-28 [Baekjoon] bfs <c++>

---
+++ graph/BFS/G5_7576.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+int graph[1000][1000];
+int visit[1000][1000];
+struct Data{
+    int x, y;
+};
+vector<Data> pos;
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+int m, n;
+int maxValue = 1;
+void breathFirstSearch()
+{
+    queue<Data> q;
+    for (int i = 0 ; i < pos.size() ; ++i)
+        q.push({pos[i].x, pos[i].y});
+    while (!q.empty())
+    {
+        Data tmp = q.front();
+        q.pop();
+
+        for (int i = 0 ; i < 4 ; ++i)
+        {
+            int nx = tmp.x + dx[i];
+            int ny = tmp.y + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || graph[nx][ny] == -1 || graph[nx][ny])
+                continue;
+            q.push({nx, ny});
+            graph[nx][ny] = graph[tmp.x][tmp.y] + 1;
+            maxValue = max(maxValue, graph[nx][ny]);
+        }
+    }
+    bool flag = true;
+    for (int i = 0 ; i < n ; ++i)
+        for (int j = 0 ; j < m ; ++j)
+            if (graph[i][j] == 0)
+                flag = false;
+    maxValue = (flag ? maxValue - 1 : -1);
+    cout << maxValue;  
+}
+int main()
+{
+    cin >> m >> n;
+    for (int i = 0 ; i < n ; ++i)
+        for (int j = 0 ; j < m ; ++j)
+        {
+            cin >> graph[i][j];
+            if (graph[i][j] == 1)
+                pos.push_back({i, j});
+        }
+    
+    breathFirstSearch();
+}
