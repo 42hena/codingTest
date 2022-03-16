52ae432c65d4c42e5ff7b5adaa3ec30875d78fab
42hena <somnium1108@gmail.com>
2022-03-16 16:54:03 +0900

    FAIL

---
+++ FailList/G4_17404.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[1000][3];//r g b
+int rgb[1000][3];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0 ; i < n ; ++i)
+    {
+        for (int j = 0 ; j < 3 ; ++j)
+            cin >> rgb[i][j];
+    }
+    for (int i = 2 ; i < n ; ++i)
+    {
+        if (i == 0)
+        {
+
+        }
+    }
+}
