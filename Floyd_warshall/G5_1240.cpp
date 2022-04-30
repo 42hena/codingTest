2f6467edfb961fe3c07248fdb6cd2f8a93b2c127
42hena <somnium1108@gmail.com>
2022-04-30 16:05:25 +0900

    2022-04-30 [BOJ] <c++>
    - link: https://www.acmicpc.net/problem/1240
    - type: Floyd-Warshall
    - level: Gold 5
    - comment: easy

---
+++ Floyd_warshall/G5_1240.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 10001
+#define INF 1e9
+int cost[MAX][MAX];
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+    int v1, v2, w;
+    for (int i = 1 ; i < MAX ; ++i)
+    {
+        for (int j = 1 ; j < MAX ; ++j)
+        {
+            cost[i][j] = INF;
+            if (i == j)
+                cost[i][j] = 0;
+        }   
+    }
+
+    for (int i = 0 ; i < n - 1 ; ++i)
+    {
+        cin >> v1 >> v2 >> w;
+        cost[v1][v2] = w;
+        cost[v2][v1] = w;
+    }
+
+    for (int k = 1 ; k < n + 1 ; ++k)
+    {
+        for (int i = 1 ; i < n + 1 ; ++i)
+        {
+            for (int j = 1 ; j < n + 1 ; ++j)
+            {
+                cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+            }   
+        }   
+    }
+
+
+    for (int i = 0 ; i < m ; ++i)
+    {
+        cin >> v1 >> v2;
+        cout << cost[v1][v2] << '\n';   
+    }
+}
