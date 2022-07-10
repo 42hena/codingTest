971b869552b462191c406233c492a28e1b814a19
42hena <somnium1108@gmail.com>
2022-07-10 22:08:42 +0900

    2022-07-10 [BOJ] <c++>
    - link: https://www.acmicpc.net/problem/1744
    - type: greedy
    - level: Gold 4
    - comment: need learn sort, greater<>, less<>

--- greedy/G4_1744.cpp
+++ greedy/G4_1744.cpp
@@ -1,35 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+
+// [n : 수열의 크기]
+// [plus :  2 이상의 값들]
+// [minus : 0 이하의 값들]
+int n;
+int main(void)
 {
-    int n;
     cin >> n;
     vector<int> plus;
     vector<int> minus;
-    int num;
-    int ans = 0;
+    int number;
+    int total = 0;
     for (int i = 0 ; i < n ; ++i)
     {
-        cin >> num;
-        if (num == 1)   
-            ans += 1;
-        else if (num > 1)
-            plus.push_back(num);
+        cin >> number;
+        if (number > 0)
+        {
+            if (number == 1)
+                total++;
+            else
+                plus.push_back(number);
+        }
         else
-            minus.push_back(num);
+            minus.push_back(number);
     }
-    sort(plus.begin(), plus.end());
     sort(minus.begin(), minus.end());
-    int plusSize = plus.size();
-    int minusSize = minus.size();
+    sort(plus.begin(), plus.end(), greater<>());
 
-    for (int i = plusSize - 1 ; i > 0 ; i = i - 2)
-        ans += plus[i] * plus[i - 1];
-    if ((plusSize & 1))
-        ans += plus[0];
-    for (int i = 0 ; i < minusSize - 1 ; i = i + 2)
-        ans += minus[i] * minus[i + 1];
-    if ((minusSize & 1))
-        ans += minus[minusSize - 1];
-    cout << ans;
+    int minusCount = minus.size();
+    for (int i = 0 ; i < minusCount ; i = i + 2)
+    {
+        if (i + 1 < minusCount)
+            total += minus[i] * minus[i + 1];
+        else
+            total += minus[i];
+    }
+    int plusCount = plus.size();
+    for (int i = 0 ; i < plusCount ; i = i + 2)
+    {
+        if (i + 1 < plusCount)
+        {
+            total += plus[i] * plus[i + 1];
+        }
+        else
+            total += plus[i];
+    }
+    cout << total;
 }
