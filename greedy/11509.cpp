4107e230a6c68a1669dfa2633029ed8b9a0eee60
42hena <somnium1108@gmail.com>
2022-08-12 18:23:39 +0900

    2022-08-12 [BOJ] <c++>
    - link: https://www.acmicpc.net/problem/1463
    - type: Greedy
    - level: Gold 5
    - comment: need refactoring

---
+++ greedy/11509.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> balloons(n);
+    for (int i = 0 ; i < n ; ++i)
+        cin >> balloons[i];
+    int cnt = 0;
+    for (int i = 0 ; i < n ; ++i)
+    {
+        int start = balloons[i];
+        if (!start)
+            continue;
+        cnt++;
+        for (int j = i ; j < n ; ++j)
+        {
+            if (start < 1)
+                break ;
+            if (start == balloons[j])
+            {
+                start -= 1;
+                balloons[j] = 0;
+            }
+        }
+        // for (int k = 0 ; k < n ; ++k)
+        //     cout << balloons[k] << ' ';
+        // cout << "\n\n";
+    }
+    cout << cnt << '\n';
+}
