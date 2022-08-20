0ae2e07a7827cb443ddc66da67a3f3a3af47f5e8
42hena <somnium1108@gmail.com>
2022-08-20 22:29:09 +0900

    2022-08-20 [BOJ] <c++>
    - link: https://www.acmicpc.net/problem/2285
    - type: Greedy
    - level: Gold 4
    - comment:

---
+++ greedy/2285.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define LL long long
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int> > v(n);
+    LL total = 0;    
+    for (int i = 0 ; i < n ; ++i)
+    {
+        cin >> v[i].first >> v[i].second;
+        total += v[i].second;
+    }
+    sort(v.begin(), v.end());
+    if (total & 1)
+    {
+        total = (total >> 1) + 1;
+    }
+    else {
+        total >>= 1;
+    }
+    for (int i = 0 ; i < n ; ++i)
+    {
+        if (total - v[i].second <= 0)
+        {
+            cout << v[i].first << '\n';
+            break;
+        }
+        total -= v[i].second;
+    }
+}
